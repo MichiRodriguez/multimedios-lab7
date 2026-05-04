e2457a1bfccf7e0ad484aec47b35ceed5a67af97
Michelle Rodriguez <michiroov@gmail.com>
2026-05-04 15:17:48 -0600

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { SidebarTree, MainContent } from "./modules/ui.js";
 customElements.define("sidebar-tree", SidebarTree);
 customElements.define("main-content", MainContent);
 
-// Inicializar con primer partido
 window.addEventListener("DOMContentLoaded", () => {
   const first = data.temporadas[0].partidos[0];
   document.querySelector("main-content").load(first);
